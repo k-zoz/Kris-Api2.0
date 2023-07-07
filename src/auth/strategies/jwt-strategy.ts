4f16c3d5a491323821238c9d2d98be4fba1260e8
k-zoz <nwachukwulogan@gmail.com>
2023-07-07 12:51:46 +0100

    same as before

--- src/auth/strategies/jwt-strategy.ts
+++ src/auth/strategies/jwt-strategy.ts
@@ -27,6 +27,8 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     const expiry = exp * 1000;
     if (Date.now() > expiry) {
       throw new AppTokenExpiredException();
+      //TODO work on the middleware , to give a custom msg if the token is expired.
+      //TODO the middleware receives and verifies the token first that's why
     }
     return { email, role };
   }
