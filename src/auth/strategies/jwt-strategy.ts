ef4665b6d2149367ba849c2a010d91093f7666db
k-zoz <nwachukwulogan@gmail.com>
2023-08-15 18:04:06 +0100

    fixed3

--- src/auth/strategies/jwt-strategy.ts
+++ src/auth/strategies/jwt-strategy.ts
@@ -8,20 +8,22 @@ import { Request } from "express";
 import { UserService } from "@back-office/user/user.service";
 import { AuthPayload } from "@core/dto/auth/auth-payload.dto";
 import { AppTokenExpiredException } from "@core/exception/app-exception";
+import * as process from "process";
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
   constructor(private readonly config: ConfigService, private readonly userService: UserService) {
     super({
       usernameField: "email",
-      ignoreExpiration: true,
+      ignoreExpiration: false,
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.ACCESS_TOKEN_SECRET
+      secretOrKey: process.env.ACCESS_TOKEN_SECRET,
+      jsonWebTokenOptions: { expiresIn: process.env.ACCESS_TOKEN_LIFETIME }
     });
   }
 
   async validate(payload) {
-    const jwtPayload: JwtPayload = payload
+    const jwtPayload: JwtPayload = payload;
     const { email, role, exp } = jwtPayload;
     return { email, role };
   }
