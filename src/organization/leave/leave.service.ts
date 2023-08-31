e3807b4811ebf83c06bc0162fad8af49a8d7c02f
k-zoz <nwachukwulogan@gmail.com>
2023-08-31 14:45:04 +0100

    fix issues

--- src/organization/leave/leave.service.ts
+++ src/organization/leave/leave.service.ts
@@ -41,9 +41,9 @@ export class LeaveService {
     await this.leaveHelperService.findOrgLeaveByName(dto.leaveName, orgID);
     const employee = await this.employeeHelperService.findEmpByEmail(userPayLoad.email);
     //date conversion based on how dates are entered or could be handled in the front end
+    dto.leaveDuration = this.utilService.calcLeaveDuration(dto.leaveStartDate, dto.leaveEndDate);
     dto.leaveEndDate = this.utilService.convertDateAgain(dto.leaveEndDate);
     dto.leaveStartDate = this.utilService.convertDateAgain(dto.leaveStartDate);
-    dto.leaveDuration = this.utilService.calcLeaveDuration(dto.leaveStartDate, dto.leaveEndDate);
     await this.leaveHelperService.leaveDurationRequest(employee, dto);
     return await this.leaveHelperService.applyLeave(dto, orgID, employee);
   }
